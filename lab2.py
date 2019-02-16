d2d2d0e1d4c608e2c7110fc6ca2e7277111b7990
charlesfu4 <ccfu@kth.se>
2019-02-16 20:23:41 +0100

    version1

--- lab2.py
+++ lab2.py
@@ -1,3 +1,19 @@
 import numpy, random, math
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+
+def start(num_sam):
+	return numpy.zeros(num_sam)
+
+def kernel(input1,input2):
+	return numpy.dot(input1,input2)
+
+def  objective(alpha):
+	return
+
+def zerofun():
+
+
+ret = minimize()
+
+alpha=['x']
